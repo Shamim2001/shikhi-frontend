4d3f14648e9fa5361a604aed6cd99cab0db14fa5
MD. Shamim Ahmed <shamimahmedridoy2016@gmail.com>
2022-10-16 21:06:25 +0600

    vue spinner added

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,14 +9,24 @@
           <div class="col-sm-12">
             <div class="courses_text">
               <h2>Courses</h2>
+              <div class="" v-show="courses == ''">
+                <dot-loader
+                  :loading="loading"
+                  :color="color"
+                  :size="size"
+                ></dot-loader>
+              </div>
             </div>
           </div>
         </div>
         <!-- .row -->
 
-        <div class="row gy-4">
-
-          <CourseItem v-for="course in courses" :key="course.id" :course="course" ></CourseItem>
+        <div class="row gy-4" v-show="courses">
+          <CourseItem
+            v-for="course in courses"
+            :key="course.id"
+            :course="course"
+          ></CourseItem>
         </div>
       </div>
     </section>
@@ -26,28 +36,31 @@
 </template>
 
 <script>
+import DotLoader from "vue-spinner/src/DotLoader.vue";
 import CourseItem from "../components/CourseItem.vue";
 import Footer from "../components/Footer.vue";
 import Header from "../components/Header.vue";
 
-import http from 'axios';
+import http from "axios";
 export default {
-  components: { Header, Footer, CourseItem },
+  components: { Header, Footer, CourseItem, DotLoader },
   data() {
     return {
-      courses: []
-    }
+      courses: [],
+    };
   },
   methods: {
     async getCourses() {
-      await http.get('http://shikhi.test/api/courses').then((res)=> {
-        this.courses = res.data.courses;
+      await http.get("http://shikhi.test/api/courses").then((res) => {
+        setTimeout(() => {
+          this.courses = res.data.courses;
+        }, 3000);
       });
-    }
+    },
   },
   mounted() {
     this.getCourses();
-  }
+  },
 };
 </script>
 
